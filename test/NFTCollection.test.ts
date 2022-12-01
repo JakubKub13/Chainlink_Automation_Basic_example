59d7cd38332ca913e91b50caf85884d2324ea221
JakubKub13 <jakubkubala3@gmail.com>
2022-12-01 10:00:39 +0100

    Preparing test file

--- test/NFTCollection.test.ts
+++ test/NFTCollection.test.ts
@@ -14,3 +14,5 @@ const NFT_MAX_SUPPLY: string = "1000";
 const NFT_MINT_COST: string = "0.1";
 const NFT_REVEAL_BATCH_SIZE: string = "5";
 const NFT_REVEAL_INTERVAL: string = "3600";
+
+function mint(){}
